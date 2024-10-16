f8e605b541b477144df4f92d8811680f61a3fe41
sergeemmanuelk <serge_em@outlook.fr>
2024-10-16 22:25:59 +0000

    implement layout

---
+++ airbnb-clone-ui/src/app/layout/navbar/category/category.model.ts
@@ -0,0 +1,10 @@
+import { IconName } from "@fortawesome/free-brands-svg-icons";
+
+export type CategoryName = "ALL" | "AMAZING_VIEWS" | "OMG" | "TREEHOUSES" | "BEACH" | "FARMS" | "TINY_HOMES" | "LAKES" | "CONTAINERS" | "CAMPING" | "CASTLE" | "SKIING" | "CAMPERS" | "ARTIC" | "BOAT" | "BED_AND_BREAKFASTS" | "ROOMS" | "EARTH_HOMES" | "TOWER" | "CAVES" | "LUXES" | "CHEFS_KITCHEN"
+
+export interface Category {
+  icon: IconName,
+  displayName: string,
+  technicalName: CategoryName,
+  activated: boolean
+}
